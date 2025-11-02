7886584a01072fc27a722819e8f2bb60cbc053e8
DIGITAL ART <eljabaryhicham@gmail.com>
2025-11-02 04:06:44 +0000

    studio-2214494506:~/studio{main}$ firebase deploy --only hosting Failed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
